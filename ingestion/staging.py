b2a4fba8fd9c9dba680f1708cb437bf7f7540e7e
stephen-githinji <kiamastephen559@gmail.com>
2026-09-07 16:36:34 +0300

    upload data to postgres

--- ingestion/staging.py
+++ ingestion/staging.py
@@ -7,5 +7,5 @@ conn_str = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{
 engine = create_engine(conn_str)
 
 
-def stage_data(data, db_name:str):
-    pd.DataFrame(data).to_sql(db_name, con=engine, if_exists="append", index=False)
+def stage_data(data, table_name:str):
+    pd.DataFrame(data).to_sql(table_name, con=engine, if_exists="append", index=False)
