1347555809cd9b985d68c3c6e0fa2e7bb8ba3c4f
stephen-githinji <kiamastephen559@gmail.com>
2026-09-15 18:30:34 +0300

    calculate volatility metrics from the ohlcv data

--- processing/volatility_metrics.py
+++ processing/volatility_metrics.py
@@ -1,14 +1,15 @@
 from pyspark.sql.window import Window
 from pyspark.sql import functions as f
+from pyspark.sql import DataFrame
 
 #calculating the high-low range for each row
 def ohlcv_volatility_transformation(ohlcv_df):
 	ohlcv_df = (ohlcv_df.withColumn('high_low_range', f.col('high')-f.col('low'))
-             .withColumn('relative_volatitlity', f.try_divide(
+             .withColumn('relative_volatility', f.try_divide(
                    f.col('high_low_range'), f.col('open'))* 100
 			 ))
 	return ohlcv_df
 
-def volatility_metrics(ohlcv_df):
+def volatility_metrics(ohlcv_df:DataFrame)->DataFrame:
     ohlcv_df = ohlcv_volatility_transformation(ohlcv_df)
     return ohlcv_df
