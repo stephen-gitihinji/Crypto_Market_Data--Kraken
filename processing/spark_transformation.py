cdd7a0b9eb46c5964dbcbe2fc1b8fd1a39876200
stephen-githinji <kiamastephen559@gmail.com>
2026-09-15 18:27:10 +0300

    synchronize the spark transformations

--- processing/spark_transformation.py
+++ processing/spark_transformation.py
@@ -1,17 +1,25 @@
 from processing.spark_schemas import ohlcv_schema, recent_spreads_schema
+from processing.spark_session import spark
 from streaming.consumers.spark_consumer import consume_topic
 from pyspark.sql import functions as f
 from processing.price_metrics import price_metrics
 from processing.volatility_metrics import volatility_metrics
 
-#consuming the kafka topics
-ohlcv_df = consume_topic("postgres-ohlcv_crypto_data", ohlcv_schema)
-recent_spreads_df = consume_topic("postgres-recent_crypto_spreads", recent_spreads_schema)
+spark = spark
+def transform_crypto_data():
+    #extract data from kafka topics
+    ohlcv_df = consume_topic(spark, "postgres-ohlcv_crypto_data")
+    recent_spreads_df = consume_topic(spark, "postgres-recent_crypto_spreads")
 
-ohlcv_df = ohlcv_df.withColumn('timestamp', f.col('timestamp').cast('timestamp'))
-recent_spreads_df = (recent_spreads_df.withColumnRenamed('time', 'timestamp')
-                     .withColumn('timestamp',f.col('timestamp').cast('timestamp')))
+    #deserialize the data
+    ohlcv_df = ohlcv_df.select(f.from_json(f.col('value').cast('string'), schema=ohlcv_schema).alias('data')).select('data.*')
+    recent_spreads_df = recent_spreads_df.select(f.from_json(f.col('value').cast('string'), schema=recent_spreads_schema).alias('data')).select('data.*')
 
-#final transformed dataframes
-ohlcv_df, spread_df = price_metrics(ohlcv_df, recent_spreads_df)
-ohlcv_df = volatility_metrics(ohlcv_df)
+    #transform the data
+    ohlcv_df = ohlcv_df.withColumn('timestamp', f.col('timestamp').cast('timestamp'))
+    recent_spreads_df = (recent_spreads_df.withColumnRenamed('time', 'timestamp')
+                        .withColumn('timestamp',f.col('timestamp').cast('timestamp')))
+
+    ohlcv_df, spread_df = price_metrics(ohlcv_df, recent_spreads_df)
+    ohlcv_df = volatility_metrics(ohlcv_df)
+    return ohlcv_df, spread_df
